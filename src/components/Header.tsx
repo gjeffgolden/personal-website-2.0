6c1fa8df34d76a309862f11489daefb7fe21c782
Jeff Golden <codebygolden@gmail.com>
2022-07-22 15:44:10 -0600

    Styling navbar and links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,26 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+import { NavigationLink } from "../styles/styles";
+
+// TODO: Hamburger menu with fun animation instead of static navbar?
 
 const Header = () => {
   return (
-    <Box>
-      <Typography>GJG</Typography>
-      <nav>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/">Home</Link>
-        <Link to="/portfolio">Portfolio</Link>
-      </nav>
+    <>
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <Typography>GJG</Typography>
+        <nav>
+          <NavigationLink to="/about">About</NavigationLink>
+          <NavigationLink to="/contact">Contact</NavigationLink>
+          <NavigationLink to="/">Home</NavigationLink>
+          <NavigationLink to="/portfolio">Portfolio</NavigationLink>
+        </nav>
+      </Box>
       <Outlet />
-    </Box>
+    </>
   );
 };
 
---
+++ src/styles/styles.ts
@@ -0,0 +1,6 @@
+import { styled } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export const NavigationLink = styled(Link)({
+  padding: 8,
+});
