2f5442ce7c8b00a7e9ed7c3c744d1c84e8131215
Jeff Golden <codebygolden@gmail.com>
2022-07-24 17:49:36 -0600

    Added profile photo

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,5 +1,19 @@
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import { PageContainer } from "../styles/styles";
 
 export const About = () => {
-  return <PageContainer>About</PageContainer>;
+  return (
+    <PageContainer>
+      <Typography variant="h5">About Me</Typography>
+      <Box>
+        <Avatar
+          alt="Jeff Golden"
+          src="/src/assets/Golden-Headshot-Casual.JPG"
+          sx={{ width: "16rem", height: "16rem" }}
+        />
+      </Box>
+    </PageContainer>
+  );
 };
