0581008ab0ad9ec24c72dd95a33737d3c1d98ea0
Jeff Golden <codebygolden@gmail.com>
2022-07-30 15:12:54 -0600

    Adjusting size on About Me pages

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -14,13 +14,13 @@ export const About = () => {
           alt="Jeff Golden"
           src="/src/assets/Golden-Headshot-Casual.JPG"
           sx={{
-            width: "16rem",
-            height: "16rem",
+            width: "12rem",
+            height: "12rem",
             float: "left",
             padding: "0px 20px 20px 0px",
           }}
         />
-        <Typography mt={8} width="42rem" variant="body2">
+        <Typography mt={5} width="34rem" variant="body1">
           I'm a frontend developer who opted out of a successful career in
           journalism and digital marketing to pursue a lifelong passion for
           computer science. Away from my computer, I'm an avid hiker, climber,
